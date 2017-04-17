5f6b783055d6fe7951b171864c978d8c6e2b55f3
Filip Lesnjakovic <filip.lesnjakovic@gmail.com>
2017-04-17 21:21:12 +0200

    app subtitle change

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -7,7 +7,7 @@ export default class App extends React.Component{
   render() {
     return(
       <div>
-        <TitleBar title={this.props.title} subtitle="Created by devFilip"/>
+        <TitleBar title={this.props.title} subtitle="Created by Filip Lesnjakovic"/>
         <div className="wrapper">
           <PlayerList players={this.props.players}/>
           <AddPlayer/>
